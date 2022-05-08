e29acdc1c13e7ec080d4ddd83e4ba0f626382cba
bluentity <103390736+bluentity@users.noreply.github.com>
2022-05-08 16:34:20 -0400

    Added Chancellor's Seal NFT
    Added a Chancellor's Seal contract (modified ERC721) and a voting contract (DaobiWithVoting_Seal) which interfaces with this contract to award the seal to the elected chancellor.  The contracts must be targeted at each other.

---
+++ contracts/DaobiWithVoting_Seal.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./DaobiVoteContract.sol";
+import "./DaobiChancellorsSeal.sol";
+
+/// @custom:security-contact jennifer.dodgson@gmail.com
+contract DAObiWithVotingWithSeal is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable, UUPSUpgradeable {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    //additions to support on-chain election of chancellor: 
+    address public chancellor; //the address of the current chancellor
+
+    // the address of the voting contract
+    //the voting contract should contain a mapping in which, given an address, the number of votes for that address (if any) can be found
+    address public votingContract; 
+    address public sealContract; //address of the Chancellor's Seal contract
+
+    //events related to voting
+    event ClaimAttempted(address _claimant, uint40 _votes);
+    event ClaimSucceeded(address _claimant, uint40 _votes);
+    event NewChancellor(address _newChanc);
+    event VoteContractChange(address _newVoteScheme);
+    event DaobiMinted(uint256 amount);
+
+    //signals that the Chancellor's seal contract has been retargeted.  
+    //The contract itself may internally; emits will be emitted from that address
+    event SealContractChange(address _newSealAddr);
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() initializer {}
+
+    function initialize() initializer public {
+        __ERC20_init("DAObiContract2", "DBT");
+        __ERC20Burnable_init();
+        __Pausable_init();
+        __AccessControl_init();
+        __UUPSUpgradeable_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _mint(msg.sender, 1000 * 10 ** decimals());
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
+    }
+
+    //THIS FUNCTION MUST BE EXECUTED IMMEDIATELY AFTER UPGRADEPROXY() TO POINT TO THE VOTE CONTRACT
+    function retargetVoting(address _voteContract) public onlyRole(PAUSER_ROLE) {
+        //pauses the contract to prevent minting and claiming after deployment until unpaused        
+        votingContract = _voteContract;
+        emit VoteContractChange(_voteContract);
+        pause();
+    }
+
+    function retargetSeal(address _sealContract) public onlyRole(PAUSER_ROLE) {
+        //pauses the contract to prevent minting and claiming after deployment until unpaused        
+        sealContract = _sealContract;
+        emit SealContractChange(_sealContract);
+        pause();
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public whenNotPaused onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+        emit DaobiMinted(amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    
+    //require holding a voting token
+    //check whether the claimant has a higher vote total than the current chancellor.  If they do, set them as current chancellor
+    function makeClaim() whenNotPaused public {
+        DaobiVoteContract dvc = DaobiVoteContract(votingContract);        
+        require (dvc.balanceOf(msg.sender) > 0, "Daobi: You don't even have a voting token!");
+        require (dvc.checkStatus(msg.sender) == true, "Daobi: You have withdrawn from service!");
+        require (dvc.assessVotes(msg.sender) > 0, "Daobi: You need AT LEAST one vote!");
+        require (msg.sender != chancellor, "You are already Chancellor!");        
+        
+        if (dvc.checkStatus(chancellor) == false) {
+            emit ClaimSucceeded(msg.sender, dvc.assessVotes(msg.sender));
+            assumeChancellorship(msg.sender);            
+        }
+        else if (dvc.assessVotes(msg.sender) > dvc.assessVotes(chancellor)) {
+            emit ClaimSucceeded(msg.sender, dvc.assessVotes(msg.sender));            
+            assumeChancellorship(msg.sender); 
+        }
+        else {
+            emit ClaimAttempted(msg.sender, dvc.assessVotes(msg.sender));
+        }
+        
+    }
+
+    //recover seal to chancellor if it's somehow missing
+    function recoverSeal() public {
+        require (msg.sender == chancellor, "Only the Chancellor can reclaim this Seal!");   
+
+        DaobiChancellorsSeal seal = DaobiChancellorsSeal(sealContract); 
+        require (seal.totalSupply() > 0, "The Seal doesn't currently exist");
+
+        seal.approve(address(this), 1);
+        seal.safeTransferFrom(seal.ownerOf(1), chancellor, 1);
+    }
+
+    function assumeChancellorship(address _newChancellor) private {
+        //this will fail if the voting contract has not been assigned the VOTE_CONTRACT role
+        DaobiChancellorsSeal seal = DaobiChancellorsSeal(sealContract); 
+        seal.approve(address(this), 1);
+        seal.safeTransferFrom(seal.ownerOf(1), _newChancellor, 1);
+
+        _revokeRole(MINTER_ROLE, chancellor);
+        chancellor = _newChancellor;
+        _grantRole(MINTER_ROLE, chancellor);
+
+        emit NewChancellor(chancellor);
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyRole(UPGRADER_ROLE)
+        override
+    {}
+
+}
